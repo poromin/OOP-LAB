8606edeb7ba0360ee5c9cd7c587fa36c427dce95
poromin <poromin93@gmail.com>
2023-06-14 06:23:53 +0700

    update branch

---
+++ Lab1/EquationSolver/src/EquationSolver/FirstDegreeEquationTwoVariables.java
@@ -0,0 +1,10 @@
+package EquationSolver;
+
+public class FirstDegreeEquationTwoVariables {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("a1x1 + a2x2 = m\nb1x1 + b2x2 = n");
+	}
+
+}
